ccdac6b5fa2e10b9ad199d2268adc0e166202f30
Jan Wojciechowski <blady992@gmail.com>
2017-01-02 22:00:54 +0100

    Init

---
+++ src/main/java/pl/lodz/ftims/pp/Application.java
@@ -0,0 +1,11 @@
+package pl.lodz.ftims.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
