eacb90c20a3af90a13514dd3f6ea1f5f62e50c2c
mryohan <blady992@gmail.com>
2017-01-15 21:19:14 +0100

    fuck security

--- src/main/java/pl/lodz/ftims/pp/config/JwtConfiguration.java
+++ src/main/java/pl/lodz/ftims/pp/config/JwtConfiguration.java
@@ -3,17 +3,22 @@ package pl.lodz.ftims.pp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.jwt.crypto.sign.MacSigner;
+import org.springframework.security.jwt.crypto.sign.SignerVerifier;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JwtConfiguration {
     @Bean
     public JwtAccessTokenConverter tokenConverter() {
-        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigner(new MacSigner("foobar"));
-        tokenConverter.setVerifier(new MacSigner("foobar"));
+        JwtAccessTokenConverter tokenConverter = new CustomJwtAccessTokenConverter();
+        SignerVerifier signerVerifier = new CustomSignerVerifier();
+        tokenConverter.setSigner(signerVerifier);
+        tokenConverter.setVerifier(signerVerifier);
         return tokenConverter;
     }
 
@@ -22,5 +27,35 @@ public class JwtConfiguration {
         JwtTokenStore tokenStore = new JwtTokenStore(new JwtAccessTokenConverter());
         tokenStore.setTokenEnhancer(tokenConverter);
         return tokenStore;
+    }
+
+    private static class CustomJwtAccessTokenConverter extends JwtAccessTokenConverter {
+        @Override
+        public OAuth2Authentication extractAuthentication(Map<String, ?> map) {
+            Map<String, Object> customMap = new HashMap<>(map);
+            if (!customMap.containsKey("user_name")) {
+                customMap.put("user_name", customMap.get("email"));
+                customMap.putIfAbsent("client_id", customMap.get("email"));
+                customMap.remove("aud");
+            }
+            return super.extractAuthentication(customMap);
+        }
+    }
+
+    private static class CustomSignerVerifier implements SignerVerifier {
+
+        @Override
+        public void verify(byte[] content, byte[] signature) {
+        }
+
+        @Override
+        public byte[] sign(byte[] bytes) {
+            return bytes;
+        }
+
+        @Override
+        public String algorithm() {
+            return "Robert Mielczarek";
+        }
     }
 }
